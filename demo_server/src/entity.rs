fd296b5d3fc5544483a2220672dedeebb2156ad4
pierre-l <pierre.larger@gmail.com>
2018-08-28 00:43:43 +0200

    Write a simple, serializable DemoEntity struct

---
+++ demo_server/src/entity.rs
@@ -0,0 +1,30 @@
+use uuid::Uuid;
+use spatiub::spatial::Entity;
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct DemoEntity{
+    id: Uuid,
+}
+
+impl Entity for DemoEntity{
+    fn id(&self) -> &Uuid {
+        &self.id
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bincode;
+
+    #[test]
+    pub fn can_serialize_entity() {
+        let entity = DemoEntity {
+            id: Uuid::new_v4(),
+        };
+
+        let serialized = bincode::serialize(&entity).unwrap();
+
+        assert_eq!(entity, bincode::deserialize(&serialized).unwrap())
+    }
+}
--- demo_server/src/main.rs
+++ demo_server/src/main.rs
@@ -1,12 +1,27 @@
+extern crate bincode;
 extern crate core;
 extern crate env_logger;
 extern crate futures;
 #[macro_use] extern crate log;
+extern crate serde;
+#[macro_use]extern crate serde_derive;
 extern crate uuid;
+extern crate spatiub;
+
+mod entity;
 
 use std::error::Error;
+use log::LevelFilter;
 
 fn main() -> Result<(), Box<Error>> {
-    println!("Hello, world!");
+    // Always print backtrace on panic.
+    ::std::env::set_var("RUST_BACKTRACE", "1");
+
+    env_logger::Builder::from_default_env()
+        .default_format_module_path(false)
+        .filter_level(LevelFilter::Info)
+        .init();
+
+    info!("Hello, world!");
     Ok(())
 }
