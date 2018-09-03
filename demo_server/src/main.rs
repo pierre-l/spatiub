e0adb2d6c76b7892da88dea81f1946d67212c202
pierre-l <pierre.larger@gmail.com>
2018-09-03 20:19:33 +0200

    Trigger new moves when one is received

--- demo_server/src/main.rs
+++ demo_server/src/main.rs
@@ -21,6 +21,8 @@ use spatiub::spatial::Point;
 use tokio::runtime::current_thread::Runtime;
 use entity::Timestamp;
 use futures::future;
+use entity::DemoEntity;
+use futures::future::FutureResult;
 
 mod entity;
 mod codec;
@@ -47,33 +49,27 @@ fn main() {
 
     thread::sleep(Duration::from_millis(100));
 
-    let client_future = client::client(&addr, |message|{
+    let client_future = client::client(&addr, move |message|{
         let result = match &message {
             Message::ConnectionAck(entity) => {
-                let mut entity = entity.clone();
-                entity.last_state_update = Timestamp::new();
-
-                let event = Message::Event(SpatialEvent {
-                    from: Point(0, 0),
-                    to: Some(Point(1, 0)),
-                    acting_entity: entity,
-                    is_a_move: true,
-                });
-                future::ok(Some(event))
+                trigger_new_move(entity.clone())
             },
             Message::Event(event) => {
                 if event.to.is_some(){
                     let latency = event.acting_entity.last_state_update.elapsed();
-                    info!("Latency: {}", latency.subsec_nanos());
 
-                    future::err(())
+                    let latency = latency.subsec_nanos();
+
+                    if latency > 1_000_000 {
+                        info!("Latency: {}", latency);
+                    }
+
+                    trigger_new_move(event.acting_entity.clone())
                 } else {
                     future::ok(None)
                 }
             },
         };
-
-        info!("Message received: {:?}", message);
         result
     });
 
@@ -81,4 +77,15 @@ fn main() {
     if let Err(_err) = runtime.block_on(client_future) {
         info!("Client stopped");
     }
+}
+
+fn trigger_new_move(mut entity: DemoEntity) -> FutureResult<Option<Message>, ()> {
+    entity.last_state_update = Timestamp::new();
+    let event = Message::Event(SpatialEvent {
+        from: Point(0, 0),
+        to: Some(Point(1, 0)),
+        acting_entity: entity,
+        is_a_move: true,
+    });
+    future::ok(Some(event))
 }
