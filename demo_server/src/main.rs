321bd3889d4f13950d2c4e260ea77b95fcfa5671
pierre-l <pierre.larger@gmail.com>
2018-09-10 20:00:51 +0200

    Move every client implementation detail to the client module

--- demo_server/src/main.rs
+++ demo_server/src/main.rs
@@ -14,31 +14,15 @@ extern crate tokio;
 extern crate tokio_codec;
 extern crate uuid;
 
-use entity::DemoEntity;
-use entity::Timestamp;
-use futures::{future, Future, stream, Stream};
 use hwloc::{CPUBIND_THREAD, CpuSet, ObjectType, Topology};
 use log::LevelFilter;
-use message::Message;
-use rand::Rng;
-use rand::thread_rng;
-use rand::ThreadRng;
-use spatiub::spatial::Entity;
 use spatiub::spatial::MapDefinition;
-use spatiub::spatial::Point;
-use spatiub::spatial::SpatialEvent;
-use std::cell::RefCell;
 use std::net::SocketAddr;
-use std::ops::Add;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
 use std::thread::JoinHandle;
 use std::time::Duration;
-use std::time::Instant;
-use tokio::runtime::current_thread::Runtime;
-use tokio::timer::Delay;
-use uuid::Uuid;
 
 mod entity;
 mod codec;
@@ -89,65 +73,11 @@ fn spawn_client_thread(
     let handle = thread::spawn(move || {
         pin_thread_to_core(hw_topo, cpu_index);
 
-        run_clients(map, addr, number_of_clients);
+        client::run_clients(map, addr, number_of_clients);
         info!("Clients stopped");
     });
 
     handle
-}
-
-fn run_clients(
-    map: MapDefinition,
-    addr: SocketAddr,
-    number_of_clients: usize
-) {
-
-    let mut iter = vec![];
-    for _i in 0..number_of_clients { iter.push(()) }
-
-    let clients = stream::iter_ok(iter)
-        .map(|_i| {
-            run_client(map.clone(), addr)
-        })
-        .buffered(number_of_clients);
-
-    let mut runtime = Runtime::new().unwrap();
-    if let Err(_err) = runtime.block_on(
-        clients.for_each(|_| {
-            future::ok(())
-        })
-    ) {
-        info!("Client stopped");
-    }
-}
-
-fn run_client(map: MapDefinition, addr: SocketAddr) -> impl Future<Item=(), Error=()> {
-    let ref addr = addr;
-    let client_entity_id = RefCell::new(None);
-
-    client::client(
-        &addr,
-        move |message| {
-            if let Message::ConnectionAck(entity) = &message {
-                client_entity_id.replace(Some(entity.id().clone()));
-            } else if let Message::Event(event) = &message {
-                if let Some(ref destination) = &event.to {
-                    let latency = event.acting_entity.last_state_update.elapsed();
-
-                    let latency = latency.subsec_nanos();
-
-                    if latency > 10_000_000 {
-                        info!("Position: {:?}, Latency: {}", destination, latency);
-                    }
-                }
-            };
-
-            if let Some(ref entity_id) = &*client_entity_id.borrow() {
-                trigger_new_move_if_client_entity_involved(message, &map, entity_id)
-            } else {
-                panic!("Expected the entity id to be set");
-            }
-        })
 }
 
 fn spawn_server_thread(
@@ -165,54 +95,6 @@ fn spawn_server_thread(
     thread::sleep(Duration::from_millis(5_000));
 
     handle
-}
-
-fn trigger_new_move_if_client_entity_involved(
-    message: Message,
-    map: &MapDefinition,
-    client_entity_id: &Uuid,
-)
-    -> Option<impl Future<Item=Message, Error=()>>
-{
-    // PERFORMANCE Suboptimal. Is there a way to avoid calling thread_rng everytime?
-    let mut rng = thread_rng();
-
-    if let Message::Event(
-        SpatialEvent{
-            from: _,
-            to: Some(to),
-            acting_entity,
-            is_a_move: true,
-        }
-    ) = message{
-        if acting_entity.id() == client_entity_id {
-            let delayed_move = trigger_new_move(&mut rng, &map, acting_entity, to);
-
-            Some(delayed_move)
-        } else {
-            None
-        }
-    } else {
-        None
-    }
-}
-
-fn trigger_new_move(rng: &mut ThreadRng, map: &MapDefinition, mut entity: DemoEntity, from: Point) -> impl Future<Item=Message, Error=()> {
-    let next_destination = map.random_point_next_to(&from, rng);
-    Delay::new(Instant::now().add(Duration::from_millis(rng.gen_range(500, 1500))))
-        .map(move |()| {
-            entity.last_state_update = Timestamp::new();
-
-            Message::Event(SpatialEvent {
-                from,
-                to: Some(next_destination),
-                acting_entity: entity,
-                is_a_move: true,
-            })
-        })
-        .map_err(|err|{
-            panic!("Timer error: {}", err)
-        })
 }
 
 fn cpuset_for_core(topology: &Topology, idx: usize) -> CpuSet {
