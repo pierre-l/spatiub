8ef367cb646f00a0d33b338eaabf5aa2bf49633f
pierre-l <pierre.larger@gmail.com>
2018-09-25 23:36:17 +0200

    Use a high priority FIFO scheduler

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo timeout 185s cset shield --exec chrt -f 99./target/release/spatiub_demo_server -- server
+sudo timeout 185s cset shield --exec chrt -f 99 ./target/release/spatiub_demo_server -- server
